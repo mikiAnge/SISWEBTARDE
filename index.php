ace7d2d95070a76c21fd5fb0a5f6d7b52c11d34d
Miguel Ruiz <mikiangel_17@hotmail.com>
2024-09-03 18:32:54 -0400

    Segundo commit actualizad de PHP y un Formulario

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lo de aqui abajo es codigo php, incluido en HTML</h1>
+    <?php echo '<p>Hello World</p>'; ?>
+    <button><a href="../SISWEBTARDE/view/operaciones.php">Operaciones</a></button><br><br>
+    <button><a href="../SISWEBTARDE/view/formulario.html">Formulario de Datos</a></button>
+</body>
+</html>
