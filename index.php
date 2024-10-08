792fdd047a5b5da6007b7f3d67001b12e06c491b
Miguel Ruiz <mikiangel_17@hotmail.com>
2024-10-08 18:25:30 -0400

    Incliur NavBar en paginas de Vistas

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/style.css">
     <title>Document</title>
 </head>
 <body>
+    <?php include 'view/panel/nav_bar.php'; ?>
     <h1>Lo de aqui abajo es codigo php, incluido en HTML</h1>
     <?php echo '<p>Hello World</p>'; ?>
     <button><a href="/view/operaciones.php">Operaciones</a></button><br><br>
     <button><a href="/view/formulario.html">Formulario de Datos</a></button>
+    <button><a href="/view/listar_datos.php">Listar Datos</a></button>
 </body>
 </html>
---
+++ view/panel/nav_bar.php
@@ -0,0 +1,9 @@
+<nav class="navbar">
+    <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="view/listar_datos.php">Listar</a></li>
+        <li><a href="view/insertar_datos.php">Insertar</a></li>
+        <li><a href="#">Ayuda</a></li>
+        <li><a href="#">Contactos</a></li>
+    </ul>
+</nav>
