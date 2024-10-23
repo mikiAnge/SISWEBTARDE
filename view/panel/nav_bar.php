f17df8f866e81ea10598054499e43d00470deb01
Daniel Ayarde Flores <vf844127@gmail.com>
2024-10-23 00:00:21 -0400

    Add files via upload

---
+++ view/panel/nav_bar.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CrackFamily</title>
+</head>
+
+<body>
+    <link rel="stylesheet" href="/diseñoWEB/css/style.css">
+    <header class="header">
+        <div class="menu container">
+
+            <a href="#" class="logo">CRACK FAMILY</a>
+            <input type="checkbox" id="menu">
+            <label for="menu">
+                <img src="/diseñoWEB/img/menu.png" class="menu-icono" alt="menu">
+            </label>
+
+            <nav class="navbar">
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Peliculas</a></li>
+                    <li><a href="#">Contacto</a></li>
+                    <li><a href="#">Soporte</a></li>
+                </ul>
+            </nav>
+            <a href="#" class="btn-1">Login</a>
+        </div>
+
+        <div class="header-content container">
+            <div class="header-1">
+                <img src="/diseñoWEB/img/venom.png" alt="">
+                <a href="#" class="btn-2">Ver ahora</a>
+            </div>
+            <div class="header-2">
+                <h1>Las mejores <br> Peliculas</h1>
+                <img src="/diseñoWEB/img/play.png" alt="">
+            </div>
+        </div>
+
+    </header>
+    <section class="movies container">
+        <h2>Peliculas mas vistas</h2>
+        <hr>
+        <div class="box-container-1">
+            <div class="box-1">
+                <div class="content">
+                    <img src="/diseñoWEB/img/1.jpg" alt="">
+                    <h3>Peliculas HD</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="box-1">
+            <div class="content">
+                <img src="/diseñoWEB/img/2.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-1">
+            <div class="content">
+                <img src="/diseñoWEB/img/3.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-1">
+            <div class="content">
+                <img src="/diseñoWEB/img/4.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-1">
+            <div class="content">
+                <img src="/diseñoWEB/img/5.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-1">
+            <div class="content">
+                <img src="/diseñoWEB/img/6.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-1">
+            <div class="content">
+                <img src="/diseñoWEB/img/7.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-1">
+            <div class="content">
+                <img src="/diseñoWEB/img/8.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="load-more" id="load-more-1">Cargar mas</div>
+    </section>
+
+    <section class="movies container">
+        <h2>Peliculas de accion</h2>
+        <hr>
+        <div class="box-container-2">
+            <div class="box-2">
+                <div class="content">
+                    <img src="/diseñoWEB/img/9.jpg" alt="">
+                    <h3>Peliculas HD</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="box-2">
+            <div class="content">
+                <img src="/diseñoWEB/img/10.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-2">
+            <div class="content">
+                <img src="/diseñoWEB/img/11.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-2">
+            <div class="content">
+                <img src="/diseñoWEB/img/12.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-2">
+            <div class="content">
+                <img src="/diseñoWEB/img/13.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-2">
+            <div class="content">
+                <img src="/diseñoWEB/img/14.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-2">
+            <div class="content">
+                <img src="/diseñoWEB/img/15.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+        <div class="load-more" id="load-more-1">Cargar mas</div>
+    </section>
+
+    <section class="movies container">
+        <h2>Peliculas de estreno</h2>
+        <hr>
+        <div class="box-container-3">
+            <div class="box-3">
+                <div class="content">
+                    <img src="/diseñoWEB/img/17.jpg" alt="">
+                    <h3>Peliculas HD</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="box-3">
+            <div class="content">
+                <img src="/diseñoWEB/img/18.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-3">
+            <div class="content">
+                <img src="/diseñoWEB/img/19.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-3">
+            <div class="content">
+                <img src="/diseñoWEB/img/20.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-3">
+            <div class="content">
+                <img src="/diseñoWEB/img/21.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-3">
+            <div class="content">
+                <img src="/diseñoWEB/img/22.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-3">
+            <div class="content">
+                <img src="/diseñoWEB/img/23.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="box-3">
+            <div class="content">
+                <img src="/diseñoWEB/img/24.jpg" alt="">
+                <h3>Peliculas HD</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+        </div>
+
+        <div class="load-more" id="load-more-1">Cargar mas</div>
+    </section>
+
+
+    <footer class="footer container">
+        <h3>Crack Family</h3>
+        <ul>
+            <li><a href="#">Inicio</a>/li>
+            <li><a href="#">Nosotros</a>/li>
+            <li><a href="#">Peliculas</a>/li>
+            <li><a href="#">Contacto</a>/li>
+            <li><a href="#">Soporte</a>/li>
+        </ul>
+
+    </footer>
+
+    <script src="/diseñoWEB/script.js"></script>
+</body>
