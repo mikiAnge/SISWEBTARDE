03bf6cd28474ba2561d6887db51c8d5765213386
Daniel Ayarde Flores <vf844127@gmail.com>
2024-10-23 00:00:36 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRACK FAMILY</title>
+    <link rel="stylesheet" href="../diseñoWEB/css/style.css">
+    < </head>
+
+<body>
+    <h1>BIENVENIDO A CRACK FAMILY</h1>
+    <?php echo '<p>Disfruta de esta Pagina espero sea de tu agrado</p>'; ?>
+    <h3>PELICULAS</h3>
+    <button><a href="../diseñoWEB/view/panel/nav_bar.php">STAR</a></button><br><br>
+    <script src="/diseñoWEB/script.js"></script>
+</body>
+<link rel="stylesheet" href="../diseñoWEB/css/style.css">
+
+</html>
