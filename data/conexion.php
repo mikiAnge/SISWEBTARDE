59af476f3e0ee74635c2503e1285d2a8790e57cc
Daniel Ayarde Flores <vf844127@gmail.com>
2024-10-22 23:58:50 -0400

    Add files via upload

---
+++ data/conexion.php
@@ -0,0 +1,19 @@
+<?php
+// si trabajamos con una base de datos de MySql, nesecitas verificar
+// que tengas habilitada la extensión de mysqli o PDO
+// revisar el archivo de configuracion de PHP llamado php.ini o httpd.conf
+$servername = "localhost";// Nombre del servidor localhost
+$username = "root";// Nombre del usuario de la base de datos, si as creado coloca ese nombre, de lo contrario solo coloca root
+$password = "";// Contraseña del usario de la base de datos
+$dbname = "test";// Nombre de la base de datos
+
+// crear conexion
+$conn = new mysqli($server, $user, $password, $db);
+
+// Verificamos que exista conexion con la Base de Datos
+if ($conn->connect_error) {
+    die("Conexión Fallida: " . $conn->connect_error);
+} else {
+    print ("Conexion Exitosa a " . $dbname);
+}
+?>
